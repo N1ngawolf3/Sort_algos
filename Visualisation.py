91024b8ab761caf571c30f2162c08ed8a35acc24
n1nga <lomakin_2003@list.ru>
2024-02-05 01:02:12 +0300

    Added selection_sort + colors for swapping els

--- Visualisation.py
+++ Visualisation.py
@@ -1,24 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from random import randint
-from sort_funcs import bubble_sort
+from sort_funcs import bubble_sort, selection_sort
 
 # Генерируем случайные данные для сортировки
-data = [randint(1, 100) for _ in range(20)]
+data = [randint(1, 100) for _ in range(50)]
 
 # Инициализация графика
 fig, ax = plt.subplots()
 ax.set_title("Bubble Sort Visualization")
 
 # Создаем объект для анимации
-bar_rects = ax.bar(range(len(data)), data, align="edge")
+bar_rects = ax.bar(range(len(data)), data)
 
 # Функция, вызываемая на каждом кадре анимации
 
 
-def update_fig(data, rects):
-    for rect, val in zip(rects, data):
+def update_fig(data_color, rects):
+    data, color = data_color
+    for rect, val, col in zip(rects, data, color):
         rect.set_height(val)
+        rect.set_color(col)
 
 
 # Создаем анимацию
@@ -26,10 +28,10 @@ anim = animation.FuncAnimation(
     fig,
     func=update_fig,
     fargs=(bar_rects,),
-    frames=bubble_sort(data),
+    frames=selection_sort(data),
     repeat=False,
     blit=False,
-    interval=10,  # Интервал между кадрами в миллисекундах
+    interval=100,  # Интервал между кадрами в миллисекундах
 )
 
 # Показываем график
--- sort_funcs.py
+++ sort_funcs.py
@@ -3,12 +3,22 @@
 def bubble_sort(data):
     for i in range(len(data)-1):
         for j in range(len(data) - i - 1):
+            color = ['red' if x == j or x == j+1 else 'blue' for x in range(len(data))]
             if data[j] > data[j + 1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
-                yield data
+                yield data, color
+
 
 def selection_sort(data):
-    pass
+    for i in range(len(data) - 1):
+        smallest = i
+        for j in range(i + 1, len(data)):
+            if data[j] < data[smallest]:
+                smallest = j
+            color = ['red' if x == i or x == smallest else 'blue' for x in range(len(data))]
+        data[i], data[smallest] = data[smallest], data[i]
+        yield data, color
+
 
 if __name__ == '__main__':
-    bubble_sort()
+    pass
